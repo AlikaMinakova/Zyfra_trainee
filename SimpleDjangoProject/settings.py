4d3b719be742305b0b435d924faa5780379a50d9
AlikaMinakova <minackowa.ali@yandex.ru>
2025-07-29 15:26:47 +0300

    rename from templates to front

--- SimpleDjangoProject/settings.py
+++ SimpleDjangoProject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "SimpleDjangoProject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": [BASE_DIR / 'front']
         ,
         "APP_DIRS": True,
         "OPTIONS": {
